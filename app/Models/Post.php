e9e614bd18134cb0bc346540f8e41c74fa328837
Danu Alamsyah Putra <152958424+DanuAlamsyah@users.noreply.github.com>
2025-08-06 15:47:34 +0700

    Refactor Post model and add migration for posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,33 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-        ['id' => 1,
-        'title' => 'Judul 1',
-        'slug' => 'judul-1',
-        'author' => 'Danu Alamsyah',
-        'date' => 'Januari 1, 2023',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti illum, suscipit, fugit ullam facilis reprehenderit velit sequi exercitationem dolores, totam ducimus neque! Excepturi eligendi, assumenda quod iusto corporis at repellat?'
-        ],
-        ['id' => 2,
-        'title' => 'Judul 2',
-        'slug' => 'judul-2',
-        'author' => 'Danu Alamsyah',
-        'date' => 'Agustus 17, 2025',
-        'body' => 'sit amet consectetur adipisicing elit. Porro voluptatibus quia commodi facere neque voluptas a sit consequatur sint illum at, nemo sapiente quae pariatur, cumque repudiandae dolorem atque ab!'
-        ],
-    ];
-    }
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return Arr::first($posts, function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
-    }
+
 }
